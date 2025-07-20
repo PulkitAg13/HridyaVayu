7a690ecb7546e935d581b4dc3c33c24c1874d4f2
PulkitAg13 <pulkit.ag13@gmail.com>
2025-07-20 19:24:55 +0530

    Update app.py

--- AI-ML-Model-main/AI-ML-Model-main/app.py
+++ AI-ML-Model-main/AI-ML-Model-main/app.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 from flask import Flask, request, jsonify
+import os
 
 # Load trained model
 model = tf.keras.models.load_model("model.keras")
@@ -105,4 +106,6 @@ def predict():
 
 # Run the app
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=7860, debug=True)
+    port = int(os.environ.get("PORT", 7860))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
